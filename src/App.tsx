4a99cf97f9451a5fde93bf4cf5842dfd163f8a97
mj <minai621@naver.com>
2024-01-02 15:05:05 +0900

    chore: root folder를 src로 설정 및 절대 경로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,6 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
