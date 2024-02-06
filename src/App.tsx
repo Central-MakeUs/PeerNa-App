d8603a1aca716108266c98b78bb274892eeff287
mj <minai621@naver.com>
2024-02-07 05:13:32 +0900

    feat: kakao link 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 import HomeScreen from 'screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
+  // useEffect(() => {
+  //   const {RNWebViewController} = NativeModules;
+  //   RNWebViewController.showWebView();
+  // }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useEffect, useRef} from 'react';
-import {KeyboardAvoidingView, StyleSheet, View} from 'react-native';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Linking,
+  Platform,
+  StyleSheet,
+  View,
+} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import WebView from 'react-native-webview';
+import WebView, {WebViewNavigation} from 'react-native-webview';
 import {LocalStorageKeys} from 'types/localStorage';
 import {WebviewBridge} from 'utils/WebviewBridge';
 import {getFcmToken, registerListenerWithFCM} from 'utils/firebase';
@@ -31,6 +38,32 @@ export default function HomeScreen() {
     }
   };
 
+  const handleShouldStartLoadWithRequest = (event: WebViewNavigation) => {
+    if (event.url.startsWith('http') || event.url.startsWith('https')) {
+      return true;
+    } else if (Platform.OS === 'android' && event.url.startsWith('intent')) {
+      // substring(7)
+      Linking.openURL(event.url).catch((err: any) => {
+        Alert.alert(
+          'error',
+          '앱 실행이 실패했습니다. 설치가 되어있지 않은 경우 설치하기 버튼을 눌러주세요.',
+        );
+        console.log(err);
+      });
+
+      return false;
+    } else {
+      Linking.openURL(event.url).catch((err: any) => {
+        Alert.alert(
+          'error',
+          '앱 실행이 실패했습니다. 설치가 되어있지 않은 경우 설치하기 버튼을 눌러주세요.',
+        );
+        console.log(err);
+      });
+      return false;
+    }
+  };
+
   useEffect(() => {
     getFcmToken();
   }, []);
@@ -47,7 +80,9 @@ export default function HomeScreen() {
       <View style={{...styles.container}}>
         <WebView
           ref={webviewRef}
-          source={{uri: 'http://localhost:5173'}}
+          source={{uri: 'https://87af-58-76-161-229.ngrok-free.app'}}
+          originWhitelist={['intent', 'http', 'https', 'kakaolink']}
+          onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
           onMessage={WebviewBridge.onMessage}
           onLoad={handleLoad}
         />
